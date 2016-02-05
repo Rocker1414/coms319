503e6a8e25e789579356898a6f91083edf21a448
Chris <cdrogers@iastate.edu>
2016-02-04 19:20:14 -0600

    updated server, waits on connections now

--- lab3/src/cs319/Server.java
+++ lab3/src/cs319/Server.java
@@ -2,13 +2,12 @@ package cs319;
 
 public class Server {
 	
-	public static int PORT = 1234;
+	public final static int PORT = 4444;
 	
 	public static void main(String[] args){
 		
-		//thread to handle connections
-		Thread connections = new Thread(new ServerConnectionThread(PORT));
-		connections.start();
+		ServerConnectionThread t = new ServerConnectionThread(PORT);
+		t.run();
 		
 	}
 }
--- lab3/src/cs319/ServerLogic.java
+++ lab3/src/cs319/ServerLogic.java
@@ -3,35 +3,37 @@ package cs319;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 //this document is for all server related threads
 
-
 //thread to handle connections
-class ServerConnectionThread implements Runnable{
-	
+class ServerConnectionThread implements Runnable {
+
 	private ServerSocket serverSocket;
 	private int port;
-	
-	public ServerConnectionThread(int port){
+
+	public ServerConnectionThread(int port) {
 		try {
 			this.port = port;
-			serverSocket = new ServerSocket(port);	
-			
+			serverSocket = new ServerSocket(port);
+
 		} catch (IOException e) {
 			System.out.println("Could not listen on port " + port);
 			System.exit(-1);
 		}
 	}
-	
+
 	@Override
-	public void run(){
-		//start listening for connection
-		while(true){
+	public void run() {
+		// start listening for connection
+		while (true) {
 			Socket clientSocket = null;
 			try {
-				clientSocket = serverSocket.accept();
 
+				System.out.println("Waiting for client to connect!");
+
+				clientSocket = serverSocket.accept();
 				Thread handler = new Thread(new ClientHandleThread(clientSocket));
 				handler.start();
 
@@ -42,20 +44,34 @@ class ServerConnectionThread implements Runnable{
 	}
 }
 
-class ClientHandleThread implements Runnable{
+class ClientHandleThread implements Runnable {
 
-	private Socket client;
+	private Socket s; // Serverside socket that connects to client
 	
-	public ClientHandleThread(Socket s){
-		client = s;
+	public ClientHandleThread(Socket s) {
+		this.s = s;
 	}
-	
+
 	@Override
 	public void run() {
-		
+		printSocketInfo(s);
+		Scanner in;
+
+		try {
+			// Read from client using socket
+			in = new Scanner(s.getInputStream());
+			String clientMessage = in.nextLine();
+
+			// Print client
+			System.out.println(clientMessage);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	
+
+	void printSocketInfo(Socket s) {
+		System.out.print("Socket on Server " + Thread.currentThread() + " ");
+		System.out.print("Server Socket Local Address: " + s.getLocalAddress() + ":" + s.getLocalPort());
+		System.out.println("Server Socket Remote Address: " + s.getRemoteSocketAddress());
+	}
 }
-
-
