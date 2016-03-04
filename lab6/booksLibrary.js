995d39478767809aea257fbc7f52d83d3cf64671
Ian Baer <itwbaer@iastate.edu>
2016-03-03 22:25:09 -0600

    almost done

--- lab6/booksLibrary.js
+++ lab6/booksLibrary.js
@@ -37,6 +37,15 @@ Library.prototype.addShelf = function(shelf){ this.shelves.push(shelf); };
 Library.prototype.getShelves = function(){ return this.shelves; };
 Library.prototype.toHTML = function(){
 	
+	//need max length
+	var maxLength = 0;
+	for(var i = 0; i < this.shelves.length; i++){
+		if(this.shelves[i].books.length > maxLength){
+			maxLength = this.shelves[i].books.length;
+		}
+	}
+	
+	
 	var output = "";
 	
 	output += "<table>";
@@ -44,24 +53,24 @@ Library.prototype.toHTML = function(){
 	for(var i = 0; i < this.shelves.length; i++){
 		output += ("<th>" + this.shelves[i].name + "</th>"); 
 	}
-	
-	var cells = [];
-	for(var i = 0; i < this.shelves.length; i++){
-		cells.push([]);
-		
-	}
-	
-	var allEmpty
-	while(
-	for(var i = 0; i < this.shelves.length; i++){
-		
-		var shelf = this.shelves[i];
-		
-		for(var j = 0; j < shelf.books.length; j++){
-			output += "<tr>";
-			output += ("<td>" + this.books[i].getTitle() + "</td>");
-			output += "</tr>";
+
+	//loop through rows of books
+	for(var i = 0; i < maxLength; i++){
+		output += "<tr>";
+		//loop through shelves
+		for(var j = 0; j < this.shelves.length; j++){
+			
+			//input a blank row if shelf is out of books
+			if(i < this.shelves[j].books.length){
+				
+				output += ("<td>" + this.shelves[j].books[i].getTitle() + "</td>");
+			}
+			else{
+				output += ("<td>" + "</td>");
+			}
+			
 		}
+		output += "</tr>";
 	}
 	
 	output += "</table>";
@@ -70,3 +79,40 @@ Library.prototype.toHTML = function(){
 };
 
 Library.prototype.display = function(){ document.getElementById("content").innerHTML = this.toHTML(); };
+
+Library.prototype.search = function(title){
+	
+	for(var i = 0; i < this.shelves.length; i++){
+		
+		var currentShelf = this.shelves[i];
+		for(var j = 0; j < currentShelf.books.length; j++){
+			
+			var currentBook = currentShelf.books[j];
+			
+			if(currentBook.getTitle() == title){
+				return i;
+			}
+			
+		}
+		
+	}
+	
+	return -1;
+	
+};
+
+Library.prototype.findBook = function(){
+	
+	var title = document.getElementById("title").value;
+	var shelf = this.search(title);
+	
+	if(shelf == -1){
+		document.getElementById("content").innerHTML = "Book not found"; 
+	}
+	else{
+		this.shelves[shelf].display();
+	}
+	
+};
+
+Library.prototype.clearView = function(){ document.getElementById("content").innerHTML = ""; };
