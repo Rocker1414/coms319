d5b501cd0047d8b3e117c304912f6906afbdce7f
Ian Baer <itwbaer@iastate.edu>
2016-03-04 01:40:26 -0600

    lab 6 done

--- lab6/booksLibrary.js
+++ lab6/booksLibrary.js
@@ -1,8 +1,17 @@
-function Book(title){
+//Book class and functions
+function Book(title, author, pages){
 	this.title = title;
+	this.author = author;
+	this.length = pages;
 }
 Book.prototype.getTitle = function(){ return this.title; };
+Book.prototype.display = function(){
+	var str = JSON.stringify(this);
+	document.getElementById("content").innerHTML = str; 
+};
 
+
+//Shelf class and functions
 function Shelf(name){
 	this.name = name;
 	
@@ -10,7 +19,7 @@ function Shelf(name){
 }
 Shelf.prototype.addBook = function(book){ this.books.push(book); };
 Shelf.prototype.getBooks = function(){ return this.books; };
-Shelf.prototype.toHTML = function(){
+Shelf.prototype.toHTML = function(pos){
 
 	var output = "";
 	
@@ -18,7 +27,7 @@ Shelf.prototype.toHTML = function(){
 	output += ("<th>" + this.name + "</th>"); 
 	for(var i = 0; i < this.books.length; i++){
 		output += "<tr>";
-		output += ("<td>" + this.books[i].getTitle() + "</td>");
+		output += ("<td><a onclick=\"library.displayBook(" + pos + "," + i + ")\" href=\"javascript:void(0);\">" + this.books[i].getTitle() + "</a></td>");
 		output += "</tr>";
 	}
 	
@@ -27,8 +36,13 @@ Shelf.prototype.toHTML = function(){
 	return output;
 	
 };
-Shelf.prototype.display = function(){ document.getElementById("content").innerHTML = this.toHTML(); };
+Shelf.prototype.display = function(pos){ document.getElementById("content").innerHTML = this.toHTML(pos); };
 
+Shelf.prototype.display
+
+
+
+//Library class and functions
 function Library(){
 	this.shelves = [];
 }
@@ -63,7 +77,8 @@ Library.prototype.toHTML = function(){
 			//input a blank row if shelf is out of books
 			if(i < this.shelves[j].books.length){
 				
-				output += ("<td>" + this.shelves[j].books[i].getTitle() + "</td>");
+				output += ("<td><a onclick=\"library.displayBook(" + j + "," + i + ")\" href=\"javascript:void(0);\">" + this.shelves[j].books[i].getTitle() + "</a></td>");
+
 			}
 			else{
 				output += ("<td>" + "</td>");
@@ -78,7 +93,7 @@ Library.prototype.toHTML = function(){
 	return output;
 };
 
-Library.prototype.display = function(){ document.getElementById("content").innerHTML = this.toHTML(); };
+Library.prototype.displayAll = function(){ document.getElementById("content").innerHTML = this.toHTML(); };
 
 Library.prototype.search = function(title){
 	
@@ -110,9 +125,74 @@ Library.prototype.findBook = function(){
 		document.getElementById("content").innerHTML = "Book not found"; 
 	}
 	else{
-		this.shelves[shelf].display();
+		this.shelves[shelf].display(shelf);
 	}
 	
 };
 
+Library.prototype.listShelves = function(){
+	
+	var output = "";
+	
+	output += "<table>";
+
+	for(var i = 0; i < this.shelves.length; i++){
+		output += "<tr>";
+		output += ("<td><a onclick=\"library.displayShelf(" + i + ")\" href=\"javascript:void(0);\">" + this.shelves[i].name + "</a></td>");
+		output += "</tr>";
+	}
+	
+	output += "</table>";
+	
+	document.getElementById("content").innerHTML = output; 
+	
+	
+	
+};
+
 Library.prototype.clearView = function(){ document.getElementById("content").innerHTML = ""; };
+
+Library.prototype.displayShelf = function(pos){
+	this.shelves[pos].display(pos);
+};
+
+Library.prototype.displayBook = function(shelf, book){
+	this.shelves[shelf].books[book].display();
+};
+
+
+//Populate library
+var library = new Library();
+
+library.addShelf(new Shelf("Shelf1"));
+
+library.addShelf(new Shelf("Shelf2"));
+
+library.addShelf(new Shelf("Shelf3"));
+
+library.shelves[0].addBook(new Book("Book1", "me", 100));
+library.shelves[0].addBook(new Book("Book1", "me", 100));
+library.shelves[0].addBook(new Book("Book1", "me", 100));
+library.shelves[0].addBook(new Book("Book2", "foo", 200));
+library.shelves[0].addBook(new Book("Book2", "foo", 200));
+library.shelves[0].addBook(new Book("Book3", "foo", 300));
+library.shelves[0].addBook(new Book("Book3", "foo", 300));
+library.shelves[0].addBook(new Book("Book3", "foo", 300));
+library.shelves[0].addBook(new Book("Book4", "me", 400));
+
+library.shelves[1].addBook(new Book("Book5", "me", 500));
+library.shelves[1].addBook(new Book("Book5", "me", 500));
+library.shelves[1].addBook(new Book("Book5", "me", 500));
+library.shelves[1].addBook(new Book("Book5", "me", 500));
+library.shelves[1].addBook(new Book("Book5", "me", 500));
+library.shelves[1].addBook(new Book("Book6", "foo", 600));
+library.shelves[1].addBook(new Book("Book6", "foo", 600));
+library.shelves[1].addBook(new Book("Book8", "foo", 800));
+library.shelves[1].addBook(new Book("Book8", "foo", 800));
+
+library.shelves[2].addBook(new Book("Book9", "me", 900));
+library.shelves[2].addBook(new Book("Book1", "me", 100));
+library.shelves[2].addBook(new Book("Book2", "foo", 200));
+library.shelves[2].addBook(new Book("Book3", "foo", 300));
+library.shelves[2].addBook(new Book("Book5", "me", 500));
+
