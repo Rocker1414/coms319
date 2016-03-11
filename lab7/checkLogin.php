894bc40753a053c84531be5376875dc2af17c662
Chris <cdrogers@iastate.edu>
2016-03-11 01:30:16 -0600

    Login working

---
+++ lab7/checkLogin.php
@@ -0,0 +1,24 @@
+<?php 
+header('Access-Control-Allow-Origin: *'); 
+// read the users.txt file and then check the user/password. 
+// return a json object with the success/failure info
+	
+	$n = $_REQUEST['name'];
+	$p = $_REQUEST['pass'];
+	
+	$file = fopen("http://localhost/lab08/users.txt", "r") or die("Unable to open file!");
+	while(!feof($file)){
+		$line = trim(fgets($file));
+		$combo = explode(" ", $line);
+		
+		$checkUser = strcmp($n, $combo[0]);
+		$checkPass = strcmp($p,$combo[1]);	
+
+		if($n === $combo[0] && $p === $combo[1]){	
+			echo json_encode("Successful Login");
+			break;
+		}	
+	}	
+	
+	fclose($file);
+?>
---
+++ lab7/viewPosts.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	</head>
+	
+	<body>
+		<p> Successfully opened viewPosts.php </p>
+	</body>
+</html>
