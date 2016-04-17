413321620d89b2247532b1fcde4e581f26b442f4
Ian Baer <itwbaer@iastate.edu>
2016-04-16 20:43:41 -0500

    posts retrieved lab 12

--- lab12/webSocket.js
+++ lab12/webSocket.js
@@ -1,27 +1,7 @@
 var io = require('socket.io').listen(5000);
+var fs = require('fs');
 
 var server = new Server();
-
-io.sockets.on('connection', function(socket) {
-
-  server.clients.push(socket);
-
-  server.sendMessage("New connection");
-
-
-});
-
-function Server(){
-  this.clients = [];
-}
-
-Server.prototype.broadcastMessage = function(message){
-  for(var i = 0; i < this.clients.length; i++){
-      var socket = this.clients[i];
-      socket.emit('server', message);
-  }
-}
-
 
 //event declaration
 /* socket.on('eventName', function(content) {
@@ -30,3 +10,76 @@ Server.prototype.broadcastMessage = function(message){
 
 //event trigger
 //socket.emit('eventName', variable);
+
+
+io.sockets.on('connection', function(socket) {
+
+  socket.on('login', function(name, password){
+
+    var valid = true;
+    if (valid){
+       var u = new User(name, this);
+       server.users.push(u);
+       this.emit('success');
+       this.emit('posts', getPosts());
+
+       server.broadcastMessage(name + " logged in.");
+    }
+
+  });
+
+});
+
+function Server(){
+  this.users = [];
+}
+
+Server.prototype.broadcastMessage = function(message){
+  for(var i = 0; i < this.users.length; i++){
+      var socket = this.users[i].socket;
+      socket.emit('broadcast', message);
+  }
+}
+
+Server.prototype.sendPosts = function(){
+  for(var i = 0; i < this.users.length; i++){
+      var socket = this.users[i].socket;
+      socket.emit('posts', getPosts());
+  }
+}
+
+function User(name, socket){
+  this.name = name;
+  this.socket = socket;
+  this.loggedIn = false;
+}
+
+function getPosts(){
+  var postData = JSON.parse(fs.readFileSync('posts.txt', 'utf8'));
+
+  var all = "";
+
+  for(var i = 0; i < postData.length; i++){
+    var current = "<div class='post'>";
+    current = current + "<h1>" + postData[i]["title"] + "</h1>";
+
+    //update link
+    //current = current + "<a onclick='update(\"" + postData[i]["title"] + "\", \"" + postData[i]["content"] + "\")' href='#'>Update</a>";
+    //author
+    current = current + "<h3>Author: " + postData[i]["author"] + "</h3>";
+    //date
+
+    current = current + "<h3>Last Updated: " + postData[i]["date"] + "</h3>";
+    
+    current = current + "<p class='content'>" + postData[i]["content"] + "</p>";
+    
+    current = current + "</div>";
+    
+    all = all + current + "<br>";
+
+
+
+  }
+
+  return all;
+}
